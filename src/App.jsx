35f8a588c478e7e9f4f78e83b2720edeb5c402f4
Duy Truong <truongbaoduy2003@gmail.com>
2024-02-20 17:29:45 +0700

    config mui theme light dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Button from "@mui/material/Button";
+import { useColorScheme } from "@mui/material/styles";
 
 function App() {
+  const { mode, setMode } = useColorScheme();
+
   return (
     <>
+      <Button
+        onClick={() => {
+          setMode(mode === "light" ? "dark" : "light");
+        }}
+      >
+        {mode === "light" ? "Turn dark" : "Turn light"}
+      </Button>
+      <hr />
       <div>Duy Truong</div>
       <Button variant="text">Hello World</Button>
       <Button variant="contained">Hello World</Button>
