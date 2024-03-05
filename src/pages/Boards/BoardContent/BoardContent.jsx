72095455f9833a4c987f5210765114cb05dcbec3
Duy Truong <truongbaoduy2003@gmail.com>
2024-03-05 17:21:03 +0700

    onDragOver

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { cloneDeep } from "lodash";
 import { Box } from "@mui/material";
 import ListColumns from "./ListColumns/ListColumns";
 import { mapOrder } from "~/utils/sorts";
@@ -48,8 +49,14 @@ const BoardContent = ({ board }) => {
     setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, "_id"));
   }, [board]);
 
+  const findColumnByCardId = (id) => {
+    return orderedColumns.find((column) =>
+      column.cards.map((card) => card._id)?.includes(id)
+    );
+  };
+
   const handleDragStart = (event) => {
-    console.log(">>> handleDragStart: ", event);
+    // console.log(">>> handleDragStart: ", event);
     setActiveDragItemId(event?.active.id);
     setActiveDragItemType(
       event?.active.data.current.columnId
@@ -59,11 +66,92 @@ const BoardContent = ({ board }) => {
     setActiveDragItemData(event?.active.data.current);
   };
 
+  const handleDragOver = (event) => {
+    if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) return;
+
+    // console.log(">>> handleDragOver: ", event);
+
+    const { active, over } = event;
+    if (!active || !over) return;
+    const {
+      id: activeDraggingCardId,
+      data: { current: activeDraggingCardData },
+    } = active;
+    const { id: overCardId } = over;
+
+    const activeColumn = findColumnByCardId(activeDraggingCardId);
+    const overColumn = findColumnByCardId(overCardId);
+
+    if (!activeColumn || !overColumn) return;
+    if (activeColumn !== overColumn) {
+      setOrderedColumns((prevColumns) => {
+        const overCardIndex = overColumn.cards.findIndex(
+          (card) => card._id === overCardId
+        );
+        console.log("overCardIndex: ", overCardIndex);
+
+        let newCardIndex;
+        const isBelowOverItem =
+          active.rect.current.translated &&
+          active.rect.current.translated.top > over.rect.top + over.rect.height;
+        const modifier = isBelowOverItem ? 1 : 0;
+        newCardIndex =
+          overCardIndex >= 0
+            ? overCardIndex + modifier
+            : overColumn?.cards.length + 1;
+
+        console.log("isBelowOverItem: ", isBelowOverItem);
+        console.log("modifier: ", modifier);
+        console.log("newCardIndex: ", newCardIndex);
+
+        const nextColumns = cloneDeep(prevColumns);
+
+        const nextActiveColumn = nextColumns.find(
+          (column) => column._id === activeColumn._id
+        );
+        const nextOverColumn = nextColumns.find(
+          (column) => column._id === overColumn._id
+        );
+
+        if (nextActiveColumn) {
+          nextActiveColumn.cards = nextActiveColumn.cards.filter(
+            (card) => card._id !== activeDraggingCardId
+          );
+          nextActiveColumn.cardOrderIds = nextActiveColumn.cards.map(
+            (card) => card._id
+          );
+        }
+
+        if (nextOverColumn) {
+          nextOverColumn.cards = nextOverColumn.cards.filter(
+            (card) => card._id !== activeDraggingCardId
+          );
+          nextOverColumn.cards = nextOverColumn.cards.toSpliced(
+            newCardIndex,
+            0,
+            activeDraggingCardData
+          );
+          nextOverColumn.cardOrderIds = nextOverColumn.cards.map(
+            (card) => card._id
+          );
+        }
+
+        return nextColumns;
+      });
+    }
+  };
+
   const handleDragEnd = (event) => {
-    //console.log(">>> handleDragEnd: ", event);
+    // console.log(">>> handleDragEnd: ", event);
+
+    if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) {
+      // console.log(">>> Dragged card!");
+      return;
+    }
+
     const { active, over } = event;
 
-    if (!over) return;
+    if (!active || !over) return;
 
     if (active.id !== over.id) {
       const oldIndex = orderedColumns.findIndex((c) => c._id === active.id);
@@ -78,9 +166,9 @@ const BoardContent = ({ board }) => {
     setActiveDragItemData(null);
   };
 
-  console.log(">>> activeDragItemId: ", activeDragItemId);
-  console.log(">>> activeDragItemType: ", activeDragItemType);
-  console.log(">>> activeDragItemData: ", activeDragItemData);
+  // console.log(">>> activeDragItemId: ", activeDragItemId);
+  // console.log(">>> activeDragItemType: ", activeDragItemType);
+  // console.log(">>> activeDragItemData: ", activeDragItemData);
 
   const dropAnimation = {
     sideEffects: defaultDropAnimationSideEffects({
@@ -96,6 +184,7 @@ const BoardContent = ({ board }) => {
     <DndContext
       sensors={sensors}
       onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
       onDragEnd={handleDragEnd}
     >
       <Box
